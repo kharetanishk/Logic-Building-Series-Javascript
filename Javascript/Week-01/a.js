1a90eb26eee18e8d2feaa009430576ade79a5916
Tanishk khare <kharetanishk18@gmail.com>
2025-07-06 22:32:56 +0530

    Missing number function

--- Javascript/Week-01/a.js
+++ Javascript/Week-01/a.js
@@ -27,3 +27,23 @@ function reverseString(string) {
 
 console.log(reverseString("Hello world"));
 console.log(reverseString("TERI MAA KI CHUT"));
+
+//find the missing number
+
+missingNumber = (...args) => {
+  let actualSum = 0;
+  const sortedNum = args.sort(function (a, b) {
+    return a - b;
+  });
+  const n = sortedNum.length + 1;
+  const Expectedsum = (n * (n + 1)) / 2;
+
+  for (let arg of args) {
+    actualSum += arg;
+  }
+
+  const missingN = Expectedsum - actualSum;
+  return missingN;
+};
+
+console.log(missingNumber(1, 2, 4, 5));
