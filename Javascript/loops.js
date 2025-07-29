60b75ab8a83ef43e90185cb9061cbe4660dd47a8
Tanishk khare <kharetanishk18@gmail.com>
2025-07-29 22:42:27 +0530

    Mastering loops

---
+++ Javascript/loops.js
@@ -0,0 +1,28 @@
+//print even numbers from 2 to 10
+//use when you know the number of iterations
+for (let i = 0; i <= 10; i += 2) {
+  // console.log(i);
+}
+
+//while loop
+//use when you donnt know the number of itereations
+
+let count = 0;
+while (count < 5) {
+  console.log(count);
+  count++;
+}
+
+// do while loop
+// use when you want to run the logic atleaast once
+
+let num = 0;
+// do {
+//   console.log("AT LEAST ONCE");
+// } while (num < 5);
+
+let num1 = 1;
+while (num1 <= 100) {
+  num1 = num1 * 2;
+}
+console.log(num1);
