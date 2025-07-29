a33722e385a234567f84a1f62d773725566853ce
Tanishk khare <kharetanishk18@gmail.com>
2025-07-29 22:51:51 +0530

    for in , for of , for each

--- Javascript/loops.js
+++ Javascript/loops.js
@@ -26,3 +26,44 @@ while (num1 <= 100) {
   num1 = num1 * 2;
 }
 console.log(num1);
+
+//for of looops used best for array or strings , best case to find values without indexing
+let fruits = ["banana", "apple", "pineapple", "strawberry"];
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+//banana
+// apple;
+// pineapple;
+// strawberry;
+
+//for in do it for keys not the object values to do it for object values use for of
+const obj = {
+  name: "tanishk",
+  age: 21,
+};
+
+for (let key in obj) {
+  console.log(`${key} is key for ${obj[key]}`);
+}
+//output -
+// name is key for tanishk
+// age is key for 21
+
+//forEach -  it is a more functional way to loop over array elemetns without brreaking it
+//no early exit possible
+
+const arraydemo = [12, 2, 3232, 323, 12, 534, 66];
+
+arraydemo.forEach((num, idx) => {
+  console.log(`${num} has this index - ${idx}`);
+});
+
+//--output
+//12 has this index - 0
+//2 has this index - 1
+// 3232 has this index - 2
+// 323 has this index - 3
+// 12 has this index - 4
+// 534 has this index - 5
+// 66 has this index - 6
