e37e455b5468be2511bc3d44bef25d4e52fbb98a
Tanishk khare <kharetanishk18@gmail.com>
2025-08-04 23:39:15 +0530

    TWO SUM problem dsa

--- Javascript/DSA/Arrays/array.js
+++ Javascript/DSA/Arrays/array.js
@@ -4,13 +4,30 @@
 // return indices of the two numbers such that they add up to target.
 // You may assume that each input has exactly one solution,
 // and you may not use the same element twice.
+
 function twoSum(nums, target) {
-  const map = {}; // value: index
+  const map = {};
 
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
-    console.log(complement);
-    console.log(`this is the value of num[i] ${nums[i]}`);
+
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i];
+    }
+
+    map[nums[i]] = i;
+  }
+}
+const ar = [5, 2, 1, 6];
+const tr = 3;
+console.log(twoSum(ar, tr));
+
+//again
+function summing(nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
 
     if (map.hasOwnProperty(complement)) {
       return [map[complement], i];
@@ -20,4 +37,6 @@ function twoSum(nums, target) {
   }
 }
 
-console.log(twoSum([2, 3, 1, 5], 6));
+const array = [6, 1, 3, 5];
+const target = 9;
+console.log(summing(array, target));
