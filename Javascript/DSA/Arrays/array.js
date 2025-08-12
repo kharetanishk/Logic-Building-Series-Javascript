17cc5cb02332d79eee2d3194b2ff379ed689cd04
Tanishk khare <kharetanishk18@gmail.com>
2025-08-12 22:47:32 +0530

    dsa problem 4 mapping  group of anagrams

--- Javascript/DSA/Arrays/array.js
+++ Javascript/DSA/Arrays/array.js
@@ -81,11 +81,41 @@ function findDuplicate(array) {
 // Description:
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
-function findAna(str) {
+function validAnagram(s, t) {
+  if (s.length !== t.length) return false;
+
+  const map1 = {};
+  const map2 = {};
+
+  for (let letter of s) {
+    map1[letter] = (map1[letter] || 0) + 1;
+  }
+
+  for (let letter of t) {
+    map2[letter] = (map2[letter] || 0) + 1;
+  }
+
+  console.log(map1, map2);
+
+  for (let key in map1) {
+    console.log(map1[key], map2[key]);
+    if (map1[key] !== map2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(validAnagram("anut", "tanu"));
+
+//Problem 4: Group Anagrams
+// Description:
+// Given an array of strings, group the anagrams together.
+//  You can return the answer in any order.
+function groupAnagrams(strs) {
   const map = {};
-  for (let word of str) {
+
+  for (let word of strs) {
     const sorted = word.split("").sort().join("");
-    // console.log(sorted);
 
     if (!map[sorted]) {
       map[sorted] = [];
@@ -96,4 +126,5 @@ function findAna(str) {
 
   return Object.values(map);
 }
-console.log(findAna(["hello", "tea", "ate"]));
+
+console.log(groupAnagrams(["tea", "ate"]));
